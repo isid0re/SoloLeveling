df300ef0fe0c1bbe245b3354404c2246bdd0d9fb
iriFiri <82155523+iriFiri@users.noreply.github.com>
2021-04-19 19:43:26 -0400

    Add files via upload

---
+++ libs/SoloLeveling/BuildFiles/amazon.pd2StartBuild.js
@@ -0,0 +1,16 @@
+/*
+ *    @filename   amazon.StartBuild.js
+ *	  @author	  isid0re
+ *    @desc       amazon build for before respecOne
+ */
+
+var build = {
+	caster: false,
+	stats: [
+		["vitality", 80], ["dexterity", 27], ["strength", 47], ["vitality", "all"],
+	],
+	skills: [
+	
+	]
+};
+
---
+++ libs/SoloLeveling/BuildFiles/sorceress.pd2StartBuild.js
@@ -0,0 +1,35 @@
+/*
+ *    @filename   	sorceress.startBuild.js
+ *	  @author	  	isid0re
+ *    @desc       	sorceress build for before respecOne
+ */
+
+var build = {
+	caster: true,
+	skillstab: 9, //lightning
+	wantedskills: [38, 42], // charged bolt, static
+	usefulskills: [40, 49], // frozen armor, lightning
+	mercAuraName: "Defiance",
+	mercAuraWanted: 104,
+	mercDiff: 1,
+	stats: [
+		["vitality", 25],
+		["strength", 15],
+		["energy", 50],
+		["vitality", 50],
+		["strength", 35],
+		["vitality", "all"]
+	],
+	skills: [
+		[38, 1], // charged Bolt
+		[37, 1], // Warmth
+		[44, 1, false], // Frost Nova
+		[38, 6], // Charge Bolt
+		[40, 1], // Frozen Armor
+		[42, 1], // Static
+		[54, 1, false], // Telekinesis
+		[54, 1, false], // Teleport
+		[38, 20], // Charged Bolt
+		[49, 20], // lightning
+	]
+};
