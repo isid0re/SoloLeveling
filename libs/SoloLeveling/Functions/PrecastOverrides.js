02dc6dcbcc23e168f0f88b6d7d1a019aaa610771
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-05-29 13:46:17 -0400

    Update PrecastOverrides.js
    -Fix barb stuttering

--- libs/SoloLeveling/Functions/PrecastOverrides.js
+++ libs/SoloLeveling/Functions/PrecastOverrides.js
@@ -85,19 +85,22 @@ Precast.doPrecast = function (force) {
 
 			Attack.weaponSwitch(this.getBetterSlot(149));
 
-			if (me.getSkill(155, 0) && (!me.getState(51) || force)) {
+			if (me.getSkill(155, 0) && (!me.getState(51) || force) && Skill.getManaCost(155) < me.mp) {
 				Skill.cast(155, 0); // Battle Command
-				delay(me.ping + 100);
-				Skill.cast(155, 0); // Cast twice. It works on itself
 
+				if (Skill.getManaCost(155) < me.mp) {
+					delay(me.ping + 20);
+					Skill.cast(155, 0); // Cast twice. It works on itself
+				}
+				
 			}
 
-			if (me.getSkill(149, 0) && (!me.getState(32) || force)) {
+			if (me.getSkill(149, 0) && (!me.getState(32) || force) && Skill.getManaCost(149) < me.mp) {
 				Skill.cast(149, 0); // Battle Orders
 
 			}
 
-			if (me.getSkill(138, 0) && (!me.getState(26) || force)) {
+			if (me.getSkill(138, 0) && (!me.getState(26) || force) && Skill.getManaCost(138) < me.mp) {
 				Skill.cast(138, 0); // Shout
 
 			}
