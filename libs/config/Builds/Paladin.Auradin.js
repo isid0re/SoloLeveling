815ffce4f5a529f5747edbcd7ce9dda08c521c10
casualBotter <60308670+theBGuy@users.noreply.github.com>
2021-06-10 19:29:51 -0400

    Update Paladin.Auradin.js
    -update belt

--- libs/config/Builds/Paladin.Auradin.js
+++ libs/config/Builds/Paladin.Auradin.js
@@ -40,6 +40,12 @@ var AutoBuildTemplate = {
 		Update: function () {
 			Config.AttackSkill = [-1, 106, 123, 106, 123, -1, -1];
 			Config.LowManaSkill = [-1, -1];
+
+			Config.BeltColumn = ["hp", "hp", "mp", "rv"];
+			Config.MinColumn[0] = Config.BeltColumn[0] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.MinColumn[1] = Config.BeltColumn[1] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.MinColumn[2] = Config.BeltColumn[2] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
+			Config.MinColumn[3] = Config.BeltColumn[3] !== "rv" ? Math.max(1, Storage.BeltSize() - 1) : 0;
 		}
 	},
 
